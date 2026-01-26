d958744abb825f61fccb768e7b9e39857bd3319d
An0n-xen <fyfybaisieamoah6@gmail.com>
2026-01-26 11:55:40 +0000

    reset done training

---
+++ yolo_inference.py
@@ -0,0 +1,11 @@
+import torch
+from ultralytics import YOLO
+
+print(f"CUDA is available: {torch.cuda.is_available()}")
+model = YOLO('yolov8x.pt')  
+
+results = model.predict('./input_file/08fd33_4.mp4', save=True)
+print(results[0])
+print("===============================")
+for box in results[0].boxes:
+    print(box)
